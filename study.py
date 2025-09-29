8c980d5afe0b84c4c391c133c4e8c12d0359f347
choisiwon <qm0430@naver.com>
2025-09-29 10:39:27 +0900

    radix sort

--- study.py
+++ study.py
@@ -1,37 +1,21 @@
-arr = [None,5,3,9,7,6,4,1,2]
-def heapify(A, k, n):
-    """
-    A[k]를 루트로 하는 부분트리를 최소힙 성질을 만족하도록 만드는 함수
-    A : 1-based 배열 (앞에 dummy 값 하나 둬야 함)
-    k : 현재 노드 인덱스
-    n : 힙 크기 (배열 길이 - 1)
-    """
-    left = 2 * k
-    right = 2 * k + 1
-
-    # 자식 중 더 작은 쪽 찾기
-    if right <= n:  # 두 자식 모두 존재
-        if A[left] < A[right]:
-            smaller = left
-        else:
-            smaller = right
-    elif left <= n:  # 왼쪽 자식만 존재
-        smaller = left
-    else:  # 자식 없음
-        return
-
-    # 부모보다 자식이 더 작으면 교환하고 내려보내기
-    if A[smaller] < A[k]:
-        A[k], A[smaller] = A[smaller], A[k]  # swap
-        heapify(A, smaller, n)  # 교환된 자리에서 다시 검사
-
-
-def build_heap(A):
-    """
-    배열 A[1..n]을 최소 힙으로 만드는 함수
-    """
-    n = len(A) - 1  # dummy 제외한 실제 원소 개수
-    for i in range(n // 2, 0, -1):  # ⌊n/2⌋ downto 1
-        heapify(A, i, n)
-build_heap(arr)
-print((arr[1:]))
+from collections import deque
+def radix_sort(arr):
+    max_val = max(arr) # 가장 큰 숫자를 찾아 최대 자릿수 계산
+    exp = 1 # 자리수를 의미하는 변수, 초기값은 1(1의자리)
+    # 최대 자리수만큼 정렬 반복
+    while max_val // exp > 0:
+    # 0 ~ 9 까지의 큐 리스트 생성
+        buckets = [deque() for _ in range(10)]
+        # 현재 자리숫에따라 버킷 재배분
+        for num in arr:
+            index = (num // exp) % 10 # 현재 자릿수에 해당하는 숫자 추출
+            buckets[index].append(num) # 해당 숫자에 맞는 버킷 추가
+        i = 0 # 버킷에 담긴 데이터를 차례대로 배열에 다시 저장
+        for bucket in buckets:
+            while bucket:
+                arr[i] = bucket.popleft() # 버킷에서 데이터를 꺼내 저장
+                i += 1
+        exp *= 10 # 다음자리수로 이동
+arr = [170,45,75,90,802,24,2,66]
+radix_sort(arr)
+print("정렬된배열",arr)
